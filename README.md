e70f496d09e1088224050f0d766e6b280f296346
MarynaPR <pryadkamaryna@gmail.com>
2021-01-22 10:04:41 -0600

    service worker file added

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # budget-tracker
+
+`npm i webpack-dev-server -D` to install worker,
+`npm run start:dev` to view on localhost:8080
--- public/js/idb.js
+++ public/js/idb.js
@@ -12,7 +12,7 @@ request.onsuccess = function (event) {
 
     if (navigator.onLine) {
         // will create later
-        // uploadBudget();
+        uploadBudget();
     }
 };
 
@@ -26,3 +26,38 @@ function saveRecord(record) {
     // add record 
     budgetObjectStore.add(record);
 }//will be in fetch(api/transactons) if .catch () executed
+
+function uploadBudget() {
+    const transaction = db.transaction(['new_budget'], 'readwrite');
+    // access your object store
+    const budgetObjectStore = transaction.objectStore('new_budget');
+    // get all records from store and set to a variable
+    const getAll = budgetObjectStore.getAll();
+    getAll.onsuccess = function () {
+        if (getAll.result.length > 0) {
+            fetch('/api/budgets', {//api/transactions
+                method: 'POST',
+                body: JSON.stringify(getAll.result),
+                headers: {
+                    Accept: 'application/json, text/plain, */*',
+                    'Content-Type': 'application/json'
+                }
+            })
+                .then(response => response.json())
+                .then(serverResponse => {
+                    if (serverResponse.message) {
+                        throw new Error(serverResponse);
+                    }
+                    const transaction = db.transaction(['new_budget'], 'readwrite');
+                    const budgetObjectStore = transaction.objectStore('new_budget');
+                    budgetObjectStore.clear();
+
+                    alert('All saved budget is submitted!');
+                })
+                .catch(err => {
+                    console.log(err);
+                });
+        }
+    };
+}
+window.addEventListener('online', uploadBudget);
